b49e7cbd3db584ee9d966e4318acf8ad7e7af033
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2024-09-19 18:07:42 +0100

    Upload Initails Files

---
+++ ATM_Service_Bussiness_Layer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Service_Bussiness_Layer
+{
+    public class Test
+    {
+        public static string SayHelloWorldFomTest()
+        {
+            Console.WriteLine("Hello Im Test Class From ATM Service");
+
+            return "Hello From Test";
+        }
+    }
+}
