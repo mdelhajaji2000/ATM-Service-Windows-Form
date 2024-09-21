00578049adf5ea46b0ce1ec15c064c2e48a495dc
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2024-09-21 18:21:24 +0100

    Finishing logic Layer of Bank Card

---
+++ ATM_Service_Bussiness_Layer/clsBankCard.cs
@@ -0,0 +1,71 @@
+﻿using ATM_Service_Data_Access_Layer;
+using BusinessTier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Service_Bussiness_Layer
+{
+    public class clsBankCard
+    {
+        public int CardId { get; private set; }
+        public int CardNumber { get; private set; }
+        public bool ActivationStatus { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int AccountNumber { get; private set; }
+
+        public int PinCode { get; set; }
+
+        public clsClientAccount AccountInfo;
+
+        private clsBankCard(int CardID, int CardNumber, int AccountNumber, bool ActiveStatus, DateTime CreationDate, int PinCode)
+        {
+            this.CardId = CardID;
+            this.CardNumber = CardNumber;
+            this.AccountNumber = AccountNumber;
+            this.ActivationStatus = ActiveStatus;
+            this.CreationDate = CreationDate;
+            this.PinCode = PinCode;
+            this.AccountInfo = clsClientAccount.Find(this.AccountNumber);
+        }
+
+        public static clsBankCard Find(int CardNumber)
+        {
+            int PinCode = -1;
+            int CardID = -1;
+            bool ActiveStatus = false;
+            DateTime CreationDate = DateTime.Now;
+            int AccNumber = -1;
+
+            if (clsBankCardData.GetCardByCardNumber(CardNumber, ref CardID, ref AccNumber, ref PinCode, ref ActiveStatus, ref CreationDate))
+                return new clsBankCard(CardID, CardNumber, AccNumber, ActiveStatus, CreationDate, PinCode);
+            else
+                return null;
+        }
+
+        public bool Save()
+        {
+
+            this.CardId = clsBankCardData.CreateBankCardinDataBase(this.CardNumber, this.CreationDate, this.ActivationStatus, this.PinCode, this.AccountNumber);
+
+            return CardId != -1;
+        }
+
+        public bool ActivateCard()
+        {
+            return clsBankCardData.ChangeActivationStatus(this.CardId, true);
+        }
+
+        public bool DesactivateCard()
+        {
+            return clsBankCardData.ChangeActivationStatus(this.CardId, false);
+        }
+
+        public bool ChangePinCode(int NewPinCode)
+        {
+            return clsBankCardData.ChangeCodePin(this.CardId, NewPinCode);
+        }
+    }
+}
--- ATM_Service_Data_Access_Layer/clsBankCardData.cs
+++ ATM_Service_Data_Access_Layer/clsBankCardData.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -42,7 +45,7 @@ namespace ATM_Service_Data_Access_Layer
             return dt;  
         }
 
-        public static bool GetCardByCardNumber(int CardNumber, ref int CardID, ref int AccountNumber, ref bool ActivationStatus, ref DateTime CreationDate)
+        public static bool GetCardByCardNumber(int CardNumber, ref int CardID, ref int AccountNumber, ref int PinCode, ref bool ActivationStatus, ref DateTime CreationDate)
         {
             bool IsFound = false;
 
@@ -117,7 +120,7 @@ namespace ATM_Service_Data_Access_Layer
             return dt;
         }
 
-        public static bool GetCardByCardID(int CradID, ref int CardNumber, ref int AccountNumber, ref bool ActivationStatus, ref DateTime CreationDate)
+        public static bool GetCardByCardID(int CradID, ref int CardNumber, ref int AccountNumber, ref int PinCode, ref bool ActivationStatus, ref DateTime CreationDate)
         {
             bool IsFound = false;
 
@@ -155,5 +158,147 @@ namespace ATM_Service_Data_Access_Layer
 
             return IsFound;
         }
+
+        public static bool ChangeActivationStatus(int CardID, bool ActiveStatus)
+        {
+            bool IsPerformed = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "Update BankCard SET ActivationStatus = @ActiveStatus WHERE CardID = @CardID";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+
+            if (ActiveStatus)
+                command.Parameters.AddWithValue("@ActiveStatus", 1);
+            else
+                command.Parameters.AddWithValue("@ActiveStatus", 0);
+
+            command.Parameters.AddWithValue("@CardID", CardID);
+
+            try
+            {
+                connection.Open();
+
+                int affectedRows = command.ExecuteNonQuery();
+
+                IsPerformed = (affectedRows == 1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsPerformed;
+        }
+
+        public static bool ChangeCodePin(int CardID, int NewCodePin)
+        {
+            bool IsPerformed = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "Update BankCard SET PINCODE = @PINCODE Where CardID = @CardID";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+            command.Parameters.AddWithValue("@CardID", CardID);
+            command.Parameters.AddWithValue("@PinCode", NewCodePin);
+
+            try
+            {
+                connection.Open();
+
+                int AffectedRwos = command.ExecuteNonQuery();
+
+                IsPerformed = (AffectedRwos == 1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsPerformed;
+        }
+
+        public static int CreateBankCardinDataBase(int CardNumber, DateTime CreationDate, bool ActivationStatus, int PinCode, int AccountNumber)
+        {
+            int ID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "Insert Into BankCard (CardNumber, AccountNumber, CreationDate, PinCode, ActivationStatus) " +
+                " Values (@CardNumber, @AccountNumber, @CreationDate, @PinCode, @ActivationStatus);";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+            command.Parameters.AddWithValue("@Cardnumber", CardNumber);
+            command.Parameters.AddWithValue("@PinCode", PinCode);
+            command.Parameters.AddWithValue("@AccountNumber", AccountNumber);
+            command.Parameters.AddWithValue("@CreationDate", CreationDate);
+
+            if (ActivationStatus)
+                command.Parameters.AddWithValue("@ActivationStatus", 1);
+            else
+                command.Parameters.AddWithValue("@ActivationStatus", 0);
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int intResult))
+                    ID = intResult;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return ID;
+        }
+
+        public static DataTable GetDesactivatedCards()
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "SELECT * From BankCard Where ActivationStatus = 1";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
     }
 }
